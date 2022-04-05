d2520405e147e46334e00a7169463f44a44257f3
Nero Zawady <nerozawady@gmail.com>
2022-04-05 09:24:52 -0800

    Setup development environment

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [new CssMinimizerPlugin()],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+});
